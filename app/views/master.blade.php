74ad419a4a0f572e856b1820ab2e9e18b1ec6f76
Usama Ahmed <uusa35@gmail.com>
2014-06-24 10:18:50 +0300

    test

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -38,7 +38,8 @@
 <body>
     <div class="container">
         <h1> {{ Session::get('locale')}}</h1>
-<!--        @include('layouts.nav')-->
+        </br>
+        @include('layouts.nav')
         @if(Session::has('message'))
         <div class="alert alert-success">
             {{ Session::get('message') }}
